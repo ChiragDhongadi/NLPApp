1530ad0bc6a88519ac0d8fe499a2c1328c527e37
Chirag Dhongadi <dhongadichirag@gmail.com>
2025-07-23 22:29:29 +0530

    Add my NLP project

---
+++ myapi.py
@@ -0,0 +1,21 @@
+import nlpcloud
+
+class API:
+
+    def __init__(self):
+        self.client = nlpcloud.Client("finetuned-llama-3-70b", "f0651b19d429ad5a4ef627d1bf87f42456449c88", gpu=True)
+
+    def sentiment_analysis(self,text):
+        self.client = nlpcloud.Client("finetuned-llama-3-70b", "f0651b19d429ad5a4ef627d1bf87f42456449c88", gpu=True)
+        response = self.client.sentiment(text,target="NLP Cloud")
+        return response
+
+    def ner(self,text):
+        self.client = nlpcloud.Client("finetuned-llama-3-70b", "f0651b19d429ad5a4ef627d1bf87f42456449c88", gpu=True)
+        response = self.client.entities(text,searched_entity="programming languages")
+        return response
+
+    def language_detection(self,text):
+        self.client = nlpcloud.Client("python-langdetect", "f0651b19d429ad5a4ef627d1bf87f42456449c88", gpu=False)
+        response = self.client.langdetection(text)
+        return response
